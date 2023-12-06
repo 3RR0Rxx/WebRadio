319731c162160552c38ee8ae4de9bf3b301bd48d
3RR0Rxx <rrrdku2@gmail.com>
2023-12-06 21:22:54 +0100

    iFrame fixed + audio

---
+++ utilities/scripts/joueur_audio.js
@@ -0,0 +1,4 @@
+doc.addEventListener("DOMContentLoaded", function() {
+    let index = parseInt(audio.dataset.audio, 10);
+    audio.src = `../utilities/assets/audio_groupe_${index}.mp3`;
+});
--- utilities/scripts/variables_locales.js
+++ utilities/scripts/variables_locales.js
@@ -1,9 +1,22 @@
 const doc = document;
 const w = window;
-const iframe = document.querySelector('.iframe');
+const iframe = doc.querySelector('.iframe');
+const player = doc.querySelector('.player');
+const audio = doc.querySelector('audio');
+const play_button = doc.querySelector('.play-btn');
+const pause_button = doc.querySelector('.pause-btn');
+const time = doc.querySelector('.time');
+const track = doc.querySelector('.track');
 var liste = doc.querySelectorAll('li');
 var langue = navigator.language;
 let iframe_href = [null, './pages/groupe_1.html', './pages/groupe_2.html', './pages/groupe_3.html',
     './pages/groupe_4.html', './pages/groupe_5.html', './pages/groupe_6.html',
     './pages/groupe_7.html', './pages/groupe_8.html',
 ];
+
+function formatageTemps(ms) {
+    var seconds = Math.floor(ms / 1000);
+    var minutes = Math.floor(seconds / 60);
+    seconds = seconds % 60;
+    return minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
+}
