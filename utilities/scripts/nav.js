21ef7506506cabcc5d291538f591692ffdbf9f2c
3RR0Rxx <rrrdku2@gmail.com>
2023-12-06 21:30:16 +0100

    Addition audio + fix Iframe

--- utilities/scripts/nav.js
+++ utilities/scripts/nav.js
@@ -1,6 +1,5 @@
 doc.addEventListener('DOMContentLoaded', function(e) {
 
-    // Ajoute et retire la classe 'actif' si un element de la liste est cliqué
     liste.forEach(function(element) {
         element.addEventListener('click', function() {
             liste.forEach(function(el) {
@@ -9,7 +8,6 @@ doc.addEventListener('DOMContentLoaded', function(e) {
             element.classList.add('actif');
         });
 
-        // Retire la classe 'actif' si l'utilsateur clique autrepart sur la page
         doc.addEventListener('click', function(e) {
             if (!e.target.closest('li')) {
                 liste.forEach(function(el) {
